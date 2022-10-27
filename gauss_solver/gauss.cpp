8547efb7c919522333101344b2392165245ebba4
severin-denisenko <devchonkavkorobchonke@gmail.com>
2022-10-27 04:57:28 +0300

    Add Legendre polynomial creation

--- gauss_solver/gauss.cpp
+++ gauss_solver/gauss.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "gauss.h"
+#include <iostream>
 
 std::tuple<std::vector<double>, double> divide_polynomial_by(const std::vector<double>& p, double c)
 {
@@ -19,5 +20,38 @@ std::tuple<std::vector<double>, double> divide_polynomial_by(const std::vector<d
     double residual = p[n - 1] + c * result[n - 2];
 
     return {result, residual};
+}
+
+std::vector<double> get_legendre_polynomial(uint32_t n)
+{
+    std::vector<double> res(n + 1);
+
+    switch (n)
+    {
+        case 0:
+            res[0] = 1;
+            break;
+        case 1:
+            res[0] = 1;
+            res[1] = 0;
+            break;
+        default:
+            std::vector<double> p_n_1 = get_legendre_polynomial(n - 1);
+            std::vector<double> p_n_2 = get_legendre_polynomial(n - 2);
+
+            // effectively multiples by x
+            p_n_1.insert(p_n_1.end(), 0);
+
+            p_n_2.insert(p_n_2.begin(), 0);
+            p_n_2.insert(p_n_2.begin(), 0);
+
+            for (uint32_t i = 0; i < n + 1; ++i)
+            {
+                res[i] = (2 * (double) n - 1) / n * p_n_1[i] - (double) (n - 1) / n * p_n_2[i];
+            }
+            break;
+    }
+
+    return res;
 }
 
--- gauss_solver/main.cpp
+++ gauss_solver/main.cpp
@@ -3,16 +3,15 @@
 
 int main()
 {
-    std::vector<double> test = {10, 0, 3, -1, 0};
 
-    auto [res, residual] = divide_polynomial_by(test, 3);
+    auto res = get_legendre_polynomial(5);
 
     std::for_each(res.begin(), res.end(), [&](const auto &item)
     {
         std::cout << item << " ";
     });
 
-    std::cout << std::endl << residual << std::endl;
+    std::cout << std::endl;
 
     return 0;
 }
